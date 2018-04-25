35ebf614aab7f890dfef75ce03de2beae96d373f
PythonSun <1074470221@qq.com>
2018-04-25 15:43:29 +0800

    sunlesu
    新增订单审批

---
+++ application/admin/controller/Inputlogisticsorder.php
@@ -0,0 +1,11 @@
+<?php
+namespace app\admin\controller;
+use think\Controller;
+
+class Inputlogisticsorder extends Controller
+{
+	/*输入物流单号渲染方法*/
+    public function inputlogisticsorder(){
+    	return '输入物流单号';
+    }
+}
