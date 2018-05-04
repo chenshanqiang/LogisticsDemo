af080395b629cae694fe8a360293c88aae229d78
hkm2018 <2249312321@qq.com>
2018-05-04 10:15:00 +0800

    hkm
    sgdgree

--- application/admin/controller/Queryreplaceconfirmorder.php
+++ application/admin/controller/Queryreplaceconfirmorder.php
@@ -19,6 +19,28 @@ class Queryreplaceconfirmorder extends Controller
 
         $uncProduct = \app\index\model\Admin::getclassinfo('dsp_logistic.unc_product','unc_product_id');
         $this->assign('unclist',$uncProduct);
+        
+        /*获取权限设置*/
+        $userinfo = \app\index\model\Admin::getsessioninfo();
+        $role_id = intval($userinfo["role_id"]);
+        $role_info = \app\index\model\Admin::queryroleinfo($role_id);
+
+        $exportgoodspower = ($role_info[0]['order_goods_permission'])&0x10;
+        $exportreplacepower = ($role_info[0]['replace_permission'])&0x10;
+        $exportborrowpower = ($role_info[0]['borrow_sample_permission'])&0x10;
+        $exportreturnpower = ($role_info[0]['return_goods_permission'])&0x10;
+        $exportpartspower = ($role_info[0]['maintain_permission'])&0x10;
+        $exportrepairpower = ($role_info[0]['fixing_permission'])&0x10;
+        $exportalternativepower = ($role_info[0]['substitute_permission'])&0x10;
+
+        $this->assign('exportgoodspower',$exportgoodspower);
+        $this->assign('exportreplacepower',$exportreplacepower);
+        $this->assign('exportborrowpower',$exportborrowpower);
+        $this->assign('exportreturnpower',$exportreturnpower);
+        $this->assign('exportpartspower',$exportpartspower);
+        $this->assign('exportrepairpower',$exportrepairpower);
+        $this->assign('exportalternativepower',$exportalternativepower);
+
         return $this->fetch();
     }
 
